2ad1a1b108314367866fec886b4f8a4cc63eed7d
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-21 19:59:43 -0600

    Can cycle throughXML from URL

--- xml/XMLFromFile.java
+++ xml/XMLFromFile.java
@@ -1,16 +1,58 @@
-import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
 
 public class XMLFromFile {
 
-	private String xmlFile;
 	private String xmlElement;
 
 	public XMLFromFile(String xmlFile) {
-		this.xmlFile = xmlFile;
+		try {
+			loadXMLFileURL(xmlFile);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public String getXmlElement() {
 		return xmlElement;
 	}
+	
+	private void loadXMLFileURL(String urlAddress) throws Exception {
+		URL url = new URL(urlAddress);
+		URLConnection connection = url.openConnection();
+		
+		Document doc = parseXML(connection.getInputStream()); //Document represents HTML/XML 
+		NodeList docNodes = doc.getElementsByTagName("title");
 
+		for(int i = 0; i < docNodes.getLength(); i++) {
+			System.out.println(docNodes.item(i).getTextContent());
+		}
+	}
+
+	private Document parseXML(InputStream stream) throws Exception {
+		DocumentBuilderFactory objDocumentBuilderFactory = null;
+		DocumentBuilder objDocumentBuilder = null;
+		Document doc = null;
+       	try
+        {
+        	objDocumentBuilderFactory = DocumentBuilderFactory.newInstance();
+            objDocumentBuilder = objDocumentBuilderFactory.newDocumentBuilder();
+
+            doc = objDocumentBuilder.parse(stream);
+        }
+        catch(Exception ex)
+        {
+            throw ex;
+        }       
+        return doc;
+    }
+	
 }
--- xml/XMLFromFileTest.java
+++ xml/XMLFromFileTest.java
@@ -1,7 +1,6 @@
 public class XMLFromFileTest {
 
 	public static void main(String [] args) {
-		XMLFromFile xmler = new XMLFromFile("test.xml");
-		System.out.println(xmler.getXmlElement());
+		XMLFromFile xmler = new XMLFromFile("http://rss.cnn.com/rss/cnn_topstories.rss");
 	}
 }
