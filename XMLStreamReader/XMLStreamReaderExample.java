bef95a4aceb8726b3f7ed2b508c18167aa4e5750
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-03-24 18:28:13 -0600

    Starting to work on using the Stream Reader

--- XMLStreamReader/XMLStreamReaderExample.java
+++ XMLStreamReader/XMLStreamReaderExample.java
@@ -1,5 +1,8 @@
+//http://tutorials.jenkov.com/java-xml/stax-xmlstreamreader.html
+
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -10,19 +13,37 @@ public class XMLStreamReaderExample {
 	public static void main(String [] args) {
 
 		XMLInputFactory factory = XMLInputFactory.newInstance();
+
 		try {
-			URL url = new URL("http://rss.cnn.com/rss/cnn_topstories.rss");
+			URL url = new URL("http://comuppets.host56.com/?feed=rss2");
 			URLConnection connection = url.openConnection();
+
 			try { 
 				XMLStreamReader streamReader =
 					factory.createXMLStreamReader(connection.getInputStream());
+
+				boolean inItemElement = false;
+				boolean inHeader = true;
+				String[] rssTags = {"title", "link", "description"};
+
 				while(streamReader.hasNext()){
+					
     				streamReader.next();
-    				if(streamReader.getEventType() == XMLStreamReader.START_ELEMENT){
-        				System.out.println(streamReader.getLocalName());
+    				if(streamReader.getEventType() == XMLStreamReader.START_ELEMENT) {
+						if (Arrays.asList(rssTags).contains(streamReader.getLocalName())
+								&& inHeader == true) {
+							inItemElement = true;
+							try {        				
+								System.out.println(streamReader.getElementText());
+							}
+							catch(XMLStreamException e) {
+							}
+						}
+						if(streamReader.getLocalName() == "item") {
+							inHeader = false;
+						}
     				}
 				}
-
 			}
 			catch(XMLStreamException e) {
 				e.printStackTrace();
